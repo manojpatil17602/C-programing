50847be8b0a18c6636ffbf8dfa8f7b8f641f5581
manojpatil17602 <manojpatil17602@gmail.com>
2022-03-22 10:35:16 +0530

    ADDED:[manoj]program for addition of 2 numbers

---
+++ ADD.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c=0;
+	clrscr();
+	printf("\n Enter the 2 numbers");
+	scanf("%d %d",&a,&b);
+	c=a+b;
+	printf("\n Addition=%d",c);
+	getch();
+}
