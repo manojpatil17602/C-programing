1a0eaaf6b75667e9495c87be1b70a9930f704058
manojpatil17602 <manojpatil17602@gmail.com>
2022-03-23 18:33:53 +0530

    ADDED[Manoj] program for ice cream bill

---
+++ BILL.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>>
+void main()
+{
+	int a,b,c=0;
+	clrscr();
+	printf("\n Enter the ice cream price");
+	scanf("%d",&a);
+	printf("\n No of ice cream");
+	scanf("%d",&b);
+	c=a*b;
+	printf("\n total bill=%d",c);
+	getch();
+}
