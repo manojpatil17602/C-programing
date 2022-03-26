e1f124dd2cadf77a4675180ad32572931f7acc36
manojpatil17602 <manojpatil17602@gmail.com>
2022-03-26 09:50:09 +0530

    ADDED:[Manoj] Program for first value to second value Multiplication

---
+++ For loop/VALUE.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,a,b,c=0;
+	clrscr();
+	printf("\n Enter the first value");
+	scanf("%d",&a);
+	printf("\n Enter the second value");
+	scanf("%d",&b);
+	for(i=1;i<=a;i++)
+	{
+		c=a+a;
+	}
+	printf("\n Multiplication=%d",c);
+	getch();
+}
