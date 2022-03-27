b60fb1d3f45e31fff4cb473fe7428867dcbdb18e
manojpatil17602 <manojpatil17602@gmail.com>
2022-03-27 17:45:34 +0530

    ADDED:[Manoj] Program for big number

---
+++ If_else/BIGNO.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b;
+	clrscr();
+	printf("\n Enter the first no:");
+	scanf("%d",&a);
+	printf("\n Enter the secondd no:");
+	scanf("%d",&b);
+	if(a>b)
+	{
+		printf("\n a is big no:");
+	}
+	else
+	{
+		printf("\n b is big no:");
+	}
+	getch();
+}
