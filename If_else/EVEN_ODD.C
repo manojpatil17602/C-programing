644add1dbc3c5db10cea3326e20e0d79ae00034b
manojpatil17602 <manojpatil17602@gmail.com>
2022-03-29 16:38:47 +0530

    ADDED:[Mnoj] program for Even and Odd number

---
+++ If_else/EVEN_ODD.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int number;
+	clrscr();
+	printf("\nEnter an intiger:");
+	scanf("%d",&number);
+	if(number%2==0)
+	{
+		printf("\n%d is an even intiger",number);
+	}
+	else
+	{
+		printf("\n%d is an odd intiger",number);
+	}
+	getch();
+}
