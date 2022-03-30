d8d61c665dc97656b7bc519a6bc7ad60bd19949c
manojpatil17602 <manojpatil17602@gmail.com>
2022-03-30 19:47:20 +0530

    ADDED:[Manoj] Program for Substraction of 2 Numbers

---
+++ SUB.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c=0;
+	clrscr();
+	printf("\nEnter the 2 no: ");
+	scanf("%d %d",&a,&b);
+	c=a-b;
+	printf("\nSubstraction=%d",c);
+	getch();
+}
